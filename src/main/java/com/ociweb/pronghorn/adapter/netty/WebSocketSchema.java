ba9f00236a16c112f0e2d290d9e45d15c6349e3f
Nathan Tippy <nathan@tippy.name>
2015-10-24 23:55:41 -0500

    update payload to only be bytes

--- src/main/java/com/ociweb/pronghorn/adapter/netty/WebSocketSchema.java
+++ src/main/java/com/ociweb/pronghorn/adapter/netty/WebSocketSchema.java
@@ -6,7 +6,7 @@ import com.ociweb.pronghorn.pipe.MessageSchema;
 public class WebSocketSchema extends MessageSchema {
  
     public final static FieldReferenceOffsetManager FROM = new FieldReferenceOffsetManager(
-            new int[]{0xc0400003,0x90800000,0xa0000000,0xc0200003,0xc0400003,0x80800000,0xa0000000,0xc0200003,0xc0400002,0x80800000,0xc0200002,0xc0400002,0x80800000,0xc0200002},
+            new int[]{0xc0400003,0x90800000,0xb8000000,0xc0200003,0xc0400003,0x80800000,0xb8000000,0xc0200003,0xc0400002,0x80800000,0xc0200002,0xc0400002,0x80800000,0xc0200002},
             (short)0,
             new String[]{"WebSocketFragment","ChannelId","Payload",null,"WebSocketFragmentForSubscribers","SubscriptionId","Payload",null,"StartPublishSubsription","SubscriptionId",null,"StopPublishSubscription","SubscriptionId",null},
             new long[]{1, 10, 12, 0, 2, 11, 12, 0, 3, 11, 0, 4, 11, 0},
@@ -25,6 +25,10 @@ public class WebSocketSchema extends MessageSchema {
     public static final int stopSubPublishIdx;
     public static final int stopSubPublishIdSize;
     
+    public static final int fieldForSingleChannelMessageChannelId;
+    public static final int fieldForSingleChannelMessagePayload;
+    
+    
     public static final WebSocketSchema instance = new WebSocketSchema();
         
     protected WebSocketSchema() {
@@ -34,7 +38,8 @@ public class WebSocketSchema extends MessageSchema {
     static {
         
         try {
-    
+            //TODO: move these into unit tests and use constants above.
+            
             forSingleChannelMessageIdx = FieldReferenceOffsetManager.lookupTemplateLocator(1, FROM);
             forSubscribersMessageIdx = FieldReferenceOffsetManager.lookupTemplateLocator(2, FROM);//WebSocketFragmentForSubscribers
             
@@ -46,6 +51,11 @@ public class WebSocketSchema extends MessageSchema {
             
             startSubPublishIdSize = FROM.fragDataSize[startSubPublishIdx];
             stopSubPublishIdSize = FROM.fragDataSize[stopSubPublishIdx];
+            
+            fieldForSingleChannelMessageChannelId =FieldReferenceOffsetManager.lookupFieldLocator(10, forSingleChannelMessageIdx, FROM);
+            fieldForSingleChannelMessagePayload = FieldReferenceOffsetManager.lookupFieldLocator(12, forSingleChannelMessageIdx, FROM);
+            
+            
                                     
             
         } catch (Throwable e) {
